5fcd22fab0507ed85b1d46bd212dc28a9cd2f9ee
juan david sanchez <Juansangon@unisabana.edu.co>
2023-04-15 22:43:28 -0500

    .

--- src/componentes/FormularioEstudiante.jsx
+++ src/componentes/FormularioEstudiante.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export const FormularioEstudiante = ({ agregar, estudianteEditar, actualizarEstudiante }) => {
     const [id, setId] = useState("");
